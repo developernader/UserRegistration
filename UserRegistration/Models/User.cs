3db93a1a99b1440723b93d942c4dff2fc1357561
Nader Saoudy <developernader@gmail.com>
2022-12-05 23:38:48 +0300

    Add project files.

---
+++ UserRegistration/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace UserRegistration.Models
+{
+    public partial class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+        public DateTime DateCreated { get; set; }
+        public string CardNumber { get; set; }
+    }
+}
---
+++ UserRegistration/Models/UserRegistrationContext.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace UserRegistration.Models
+{
+    public partial class UserRegistrationContext : DbContext
+    {
+        public UserRegistrationContext()
+        {
+        }
+
+        public UserRegistrationContext(DbContextOptions<UserRegistrationContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<User> User { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.Property(e => e.CardNumber)
+                    .IsRequired()
+                    .HasMaxLength(16);
+
+                entity.Property(e => e.DateCreated).HasColumnType("smalldatetime");
+
+                entity.Property(e => e.Name).HasMaxLength(200);
+
+                entity.Property(e => e.Password).HasMaxLength(300);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
